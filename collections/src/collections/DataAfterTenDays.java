62a5da11f78f92d0f5323889cc24bf5287742da6
divya-07-04 <divyavarma2122@gmail.com>
2025-07-29 11:19:39 +0530

    add collections

---
+++ collections/src/collections/DataAfterTenDays.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DataAfterTenDays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter today's date (yyyy-mm-dd):");
+        LocalDate today = LocalDate.parse(scanner.nextLine());
+        LocalDate afterTenDays = today.plusDays(10);
+        
+        System.out.println("Today's date: " + today);
+        System.out.println("Date after ten days: " + afterTenDays);
+        scanner.close();
+    }
+}
